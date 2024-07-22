18a16c7cdefc109820c36074d51398bbe6c975d4
Zach Slaton <knxtdoor@aol.com>
2024-07-22 19:40:58 -0400

    add level end orb

--- Assets/Scripts/Level/EndCollision.cs
+++ Assets/Scripts/Level/EndCollision.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 
 public class EndCollision : MonoBehaviour
 {
+
+    private List<string> levelNames = new List<string>(){
+        "Luting_map",
+        "ZachLevel",
+        "WillLevel",
+        "SagarLevel"
+    };
+    public int currLevel = 0;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene("TitleScreen");
+            SceneManager.LoadScene(levelNames[currLevel + 1]);
             Time.timeScale = 1f;
-            Cursor.lockState = CursorLockMode.None;
         }
     }
 }
